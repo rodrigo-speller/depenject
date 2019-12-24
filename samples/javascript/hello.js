f7c9ffcabac2709c93a996f9350ef6483771911e
Rodrigo Speller <rodrigo@primosti.com.br>
2019-12-23 21:09:14 -0300

    Add hello samples

---
+++ samples/javascript/hello.js
@@ -0,0 +1,61 @@
+let { Container, ContainerBuilder, resolver } = require('depenject');
+
+///
+/// Define types
+///
+
+class HelloService {
+  /**
+   * @type {NameProvider}
+   */
+  nameProvider;
+
+  /**
+   * @param {NameProvider} nameProvider 
+   */
+  constructor(nameProvider) {
+    this.nameProvider = nameProvider;
+  }
+
+  /**
+   * @param {Container} container 
+   */
+  static [resolver](container) {
+    return new HelloService(container.resolve(NameProvider));
+  }
+
+  sayHello() {
+    console.log("Hello " + this.nameProvider.name + "!")
+  }
+}
+
+class NameProvider {
+  /**
+   * @type {string}
+   */
+  name = "World";
+
+  static [resolver]() {
+    return new NameProvider();
+  }
+}
+
+///
+/// Sample code
+///
+
+let container = new ContainerBuilder()
+  .register(HelloService)
+  .registerSingleton(NameProvider)
+  .build();
+
+// resolve dependencies
+
+let helloService = container.resolve(HelloService);
+let nameProvider = container.resolve(NameProvider);
+
+// execution
+
+helloService.sayHello();
+nameProvider.name = "Rodrigo Speller"
+helloService.sayHello();
---
+++ samples/typescript/hello.ts
@@ -0,0 +1,49 @@
+import { Container, ContainerBuilder, resolver } from 'depenject';
+
+///
+/// Define types
+///
+
+class HelloService {
+  private nameProvider: NameProvider;
+
+  constructor(nameProvider: NameProvider) {
+    this.nameProvider = nameProvider;
+  }
+
+  static [resolver](container: Container) {
+    return new HelloService(container.resolve(NameProvider));
+  }
+
+  sayHello() {
+    console.log("Hello " + this.nameProvider.name + "!")
+  }
+}
+
+class NameProvider {
+  public name: string = "World";
+
+  static [resolver]() {
+    return new NameProvider();
+  }
+}
+
+///
+/// Sample code
+///
+
+let container = new ContainerBuilder()
+  .register(HelloService)
+  .registerSingleton(NameProvider)
+  .build();
+
+// resolve dependencies
+
+let helloService = container.resolve(HelloService);
+let nameProvider = container.resolve(NameProvider);
+
+// execution
+
+helloService.sayHello();
+nameProvider.name = "Rodrigo Speller"
+helloService.sayHello();
